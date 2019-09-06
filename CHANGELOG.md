a0b13a42651380f2abad2ba5315340faf19433c2
MrFox <gyussz@live.com>
2019-09-06 14:54:32 +0200

    Return 0 for adjusted XP if the character is lvl 60, update toc

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,13 +1,17 @@
-# LibQuestXP - Changelog
+## LibQuestXP - Changelog
 
-## 1.0.3
+**1.0.4**
+
+- Return 0 as adjusted XP if the character is level 60
+
+**1.0.3**
 
 - Add LibQuestXP.xml for easier embedding
 
-## 1.0.2
+**1.0.2**
 
 - Change XP calculation math
 
-## 1.0.1
+**1.0.1**
 
 - Initial release
--- LibQuestXP.lua
+++ LibQuestXP.lua
@@ -1,4 +1,4 @@
-local MAJOR, MINOR = "LibQuestXP-1.0", 1
+local MAJOR, MINOR = "LibQuestXP-1.0", 4
 local LibQuestXP = LibStub:NewLibrary(MAJOR, MINOR)
 
 if not LibQuestXP then
@@ -22,6 +22,9 @@ end
 
 function LibQuestXP:GetAdjustedXP(xp, qLevel)
     local charLevel = UnitLevel("player");
+    if (charLevel == 60) {
+        return 0;
+    }
 
     local diffFactor = 2 * (qLevel - charLevel) + 20;
     if (diffFactor < 1) then
