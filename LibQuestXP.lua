adc2646d863a3469e65bc94a8ddf5516b6739504
MrFox <gyussz@live.com>
2024-04-15 08:32:09 +0200

    Merge WotLK and Classic versions, update DBs

--- LibQuestXP.lua
+++ LibQuestXP.lua
@@ -1,4 +1,4 @@
-local MAJOR, MINOR = "LibQuestXP-3.0", 2
+local MAJOR, MINOR = "LibQuestXP-3.1", 13
 local LibQuestXP = LibStub:NewLibrary(MAJOR, MINOR)
 
 if _G.WOW_PROJECT_ID == _G.WOW_PROJECT_MAINLINE then
@@ -26,7 +26,7 @@ end
 
 function LibQuestXP:GetAdjustedXP(xp, qLevel)
     local charLevel = UnitLevel("player");
-    if (charLevel == 80) then
+    if (charLevel == GetMaxPlayerLevel()) then
         return 0;
     end
 
